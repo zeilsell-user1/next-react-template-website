88fdf8258c6e47243f18840bcde5dfc733617f00
Richard George <54445865+zeilsell-user1@users.noreply.github.com>
2022-12-15 20:03:59 +0000

    Rjg add firebase hosting (#9)
    * added firebase hosting
    
    * updated to use yarn
    
    * update to yml
    
    * updated actions
    
    * udated release scripts
    
    * updated scripts
    
    * cleaned up action scripts
    
    * corrected token
    
    * removed spurious hyphen
    
    * made build direcotr the output location
    
    * removed firebase
    
    Co-authored-by: Richard George <richard.jhn.george.3@gmail.com>

--- next.config.js
+++ next.config.js
@@ -5,8 +5,12 @@ const nextConfig = {
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
+<<<<<<< HEAD
+  }
+=======
   },
   distDir: 'build',
+>>>>>>> e63f0e0c542f9befb775ba71b9f525a637d24f0e
 }
 
 module.exports = nextConfig
